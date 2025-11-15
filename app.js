168bf15e3d165b4ffdc2401c0d781d9daf53c76e
sushanthacharya2003 <acharyasushanth37@gmail.com>
2025-11-15 19:26:23 +0530

    setup the rangebuttons

--- app.js
+++ app.js
@@ -18,8 +18,46 @@ async function initApp() {
 }
 
 async function loadStockList() {
-    // will fill later
+    const list=await getStockList();
+    const symbols=Object.keys(list);
+    renderStockList(symbols,(symbol)=>{
+    currentSymbol = symbol;
+    highlightStock(symbol);
+    loadStock(symbol);
+    });
+    currentSymbol = symbols[0]
+    highlightStock(currentSymbol)
+    loadStock(currentSymbol)
 
+}
+
+//range_buttons
+
+// Select all range buttons INSIDE the range-buttons container
+const rangeButtons = document.querySelectorAll(".range-buttons button");
+
+function setupRangeButtons() {
+    rangeButtons.forEach(btn => {
+        btn.addEventListener("click", () => {
+
+            // update the global range
+            currentRange = btn.dataset.range;
+
+            // reload chart for the currently selected stock
+            loadStock(currentSymbol);
+
+            // highlight the current button
+            highlightRangeButton(btn);
+        });
+    });
+}
+
+function highlightRangeButton(activeBtn) {
+    rangeButtons.forEach(btn => {
+        btn.classList.remove("active");      // remove active state
+    });
+
+    activeBtn.classList.add("active");       // add active to clicked button
 }
 
 async function loadStock(symbol) {
